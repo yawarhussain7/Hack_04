df056700ff02adca89881855d5460a072a6fa87a
= <=>
2023-10-22 08:05:40 -0700

    Minor debugging and create required variables according to given statement

--- hack-4.c
+++ hack-4.c
@@ -4,8 +4,12 @@ Auhtor Yawar Hussain
 
 #include<stdio.h>
 
-int main(int argv, char **argc)
+int main(int argc, char **argv)
 {
-  
+    double initialBalance = atof(argv[1]);
+    double monthlyContribution = atof(argv[2]);
+    double annualRateOfReturn = atof(argv[3]);
+    double annualInflationRate = atof(argv[4]);
+    int Number_of_years = atoi(argv[5]);
     return 0;
 }
